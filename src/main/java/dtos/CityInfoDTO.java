5f7118b18154c347b34e832be1f3beb1b60a2dc3
Christoffer Nielsen <cph-cn332@cphbusiness.dk>
2022-11-25 11:05:17 +0100

    Address and cityinfo DTO

--- src/main/java/dtos/CityInfoDTO.java
+++ src/main/java/dtos/CityInfoDTO.java
@@ -1,9 +1,72 @@
 package dtos;
 
+import entities.CityInfo;
+import entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class CityInfoDTO {
+
+
     private Integer zipCode;
     private String cityName;
 
+    public CityInfoDTO(Integer zipCode, String cityName) {
+        this.zipCode = zipCode;
+        this.cityName = cityName;
+    }
 
+    public CityInfoDTO(CityInfo cityInfo){
+        this.zipCode = cityInfo.getZipCode();
+        this.cityName = cityInfo.getCityName();
+    }
 
+    public static List<CityInfoDTO> getCityInfoDTOs(List<CityInfo> cityInfos){
+        List<CityInfoDTO> cityInfoDTOs = new ArrayList<>();
+        cityInfos.forEach(cityInfo->cityInfoDTOs.add(new CityInfoDTO(cityInfo)));
+        return cityInfoDTOs;
+    }
+
+    public CityInfo getEntity(){
+        return new CityInfo(this.zipCode,this.cityName);
+    }
+
+    public Integer getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CityInfoDTO)) return false;
+        CityInfoDTO that = (CityInfoDTO) o;
+        return getZipCode().equals(that.getZipCode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getZipCode());
+    }
+
+    @Override
+    public String toString() {
+        return "CityInfoDTO{" +
+                "zipCode=" + zipCode +
+                ", cityName='" + cityName + '\'' +
+                '}';
+    }
 }
