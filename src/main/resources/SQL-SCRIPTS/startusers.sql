a2d95cad074a6d60b7e982d1735cb5dec6444276
Mark Lundgaard <marklundgaard7@gmail.com>
2022-11-25 10:40:30 +0100

    user

---
+++ src/main/resources/SQL-SCRIPTS/startusers.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO `3sem_exam`.`address` (`street_address`, `zipcode`) VALUES ('Nørgaardsvej 30', '2800');
+INSERT INTO `3sem_exam`.`address` (`street_address`, `zipcode`) VALUES ('Nørgaardsvej 25', '2800');
+
+INSERT INTO user
+VALUES ('admin', 'personaltrainer@gmail.com', 'test123', 1);
+
+INSERT INTO user
+VALUES ('user', 'testuser@gmail.com', 'test123', 2);
