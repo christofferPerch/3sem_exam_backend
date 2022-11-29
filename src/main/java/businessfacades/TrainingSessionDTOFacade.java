309a56018d4fff3ef8cf070150e8e1bd2d41bad9
R <90389727+notebrr@users.noreply.github.com>
2022-11-29 12:05:40 +0100

    API key removed

--- src/main/java/businessfacades/TrainingSessionDTOFacade.java
+++ src/main/java/businessfacades/TrainingSessionDTOFacade.java
@@ -17,6 +17,7 @@ import javax.persistence.EntityManagerFactory;
 
 public class TrainingSessionDTOFacade {
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    private static final String APK_KEY = ""; //API KEY MUST BE SAVED IN SEPERATED FOLDER AND NOT BE DEPLOYED TO GITHUB!!!!!
 
     private UserDTOFacade userFacade = UserDTOFacade.getInstance(EMF);
     private static TrainingSessionDTOFacade instance;
@@ -43,8 +44,8 @@ public class TrainingSessionDTOFacade {
             if (trainingSession.getId().equals(trainingSessionId)) {
                 String YOUR_DOMAIN_NAME = "sandbox06d065eea4cc4b92bbc153a5d24516e9.mailgun.org";
                 HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + YOUR_DOMAIN_NAME + "/messages")
-                        .basicAuth("api", "433fc8862f7dd5ad0b1333f6697e40a5-69210cfc-66acf477")
-                        .queryString("from", "You have an upcoming training session! rehman@lyngbys.me")
+                        .basicAuth("api", APK_KEY)
+                        .queryString("from", "You have an upcoming training session! trainer@lyngbys.me")
                         .queryString("to", user.getUserEmail())
                         .queryString("subject", "You have an upcoming training session!")
                         .queryString("text", "Training session starting at " + trainingSession.getTime() + " on date " + trainingSession.getTime())
