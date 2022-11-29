2695f0343a389e8c0003c9bb38f8e8b8764fd35b
R <90389727+notebrr@users.noreply.github.com>
2022-11-29 10:17:04 +0100

    Simplified function

--- src/main/java/businessfacades/TrainingSessionDTOFacade.java
+++ src/main/java/businessfacades/TrainingSessionDTOFacade.java
@@ -38,20 +38,19 @@ public class TrainingSessionDTOFacade {
     }
 
     public String sendEmailToAllUsers(int trainingSessionId) throws UnirestException, API_Exception {
-        for (User user : trainingSessionFacade.getTrainingSession(trainingSessionId).getUsers()) {
-            for (TrainingSession t : user.getTrainingSessions()) {
-                if(t.getId().equals(trainingSessionId)){
-                    String YOUR_DOMAIN_NAME = "sandbox06d065eea4cc4b92bbc153a5d24516e9.mailgun.org";
-                    HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + YOUR_DOMAIN_NAME + "/messages")
-                            .basicAuth("api", "433fc8862f7dd5ad0b1333f6697e40a5-69210cfc-66acf477")
-                            .queryString("from", "You have an upcoming training session! rehman@lyngbys.me")
-                            .queryString("to", user.getUserEmail())
-                            .queryString("subject", "You have an upcoming training session!")
-                            .queryString("text", "Training session starting at " +t.getTime() + " on date " + t.getTime())
-                            .asJson();
-                    System.out.println(user.getUserEmail());
-                }
+        TrainingSession trainingSession = trainingSessionFacade.getTrainingSession(trainingSessionId);
+        for (User user : trainingSession.getUsers()) {
+            if (trainingSession.getId().equals(trainingSessionId)) {
+                String YOUR_DOMAIN_NAME = "sandbox06d065eea4cc4b92bbc153a5d24516e9.mailgun.org";
+                HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + YOUR_DOMAIN_NAME + "/messages")
+                        .basicAuth("api", "433fc8862f7dd5ad0b1333f6697e40a5-69210cfc-66acf477")
+                        .queryString("from", "You have an upcoming training session! rehman@lyngbys.me")
+                        .queryString("to", user.getUserEmail())
+                        .queryString("subject", "You have an upcoming training session!")
+                        .queryString("text", "Training session starting at " + trainingSession.getTime() + " on date " + trainingSession.getTime())
+                        .asJson();
             }
+
         }
         return "Done";
     }
